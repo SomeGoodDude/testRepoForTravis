9031dedd2e8ec2e8e6871ac486ed60d980560c9e
Stanislav Sover <sover.stanislav@volcanoideas.com>
2015-02-04 17:42:24 +0300

    add junit support and simple test cases

--- src/com/ci/InitialClass.java
+++ src/com/ci/InitialClass.java
@@ -3,7 +3,9 @@ package com.ci;
 public class InitialClass {
     public static void main(String... args) {
         System.out.print("Hello from built code");
-        Object nullO = null;
-        nullO.hashCode();
+    }
+
+    public boolean isTestPassed() {
+        return true;
     }
 }
